e3c04579d797bacfb19bfe24437399e7ea55a085
Ryan O'Dea <70209371+ryan-odea@users.noreply.github.com>
2022-01-02 11:46:12 -0500

    entry commit

---
+++ floRal/R/colors.R
@@ -0,0 +1,40 @@
+#' Complete List of Palettes
+#'
+#' Use names(floral_palettes) to return current list of palette names
+#' Use \code{\link{floral.brewer}} to construct palettes
+#'
+#' @export
+floral_palettes <- list(
+  Rose = list(c("FF9CB1", "#FF7E92", "#FF6479", "#FF4F64", "#D33F4F")),
+  SilverUrn = list(c("#FCBED6", "#F7D7E4", "#CECED6", "#DBF0CE", "#889488")),
+  Poppy = list(c("#FC6944", "#EB362B", "#47464C", "#3E3039", "#1E191B")),
+  Hyacinth = list(c("#2E4A22", "#64883C", "#A29E92", "#CAC3AC", "#FFF7F5", "#EFCEE3", "#D793BC", "#BD87C1", "#A064AC", "#723782")),
+  Daisy = list(c("#E1E1DF", "#C0C2BD", "#C7A861", "#D49626")),
+  Daffodil = list(c("#C8B62E", "#BD8B07", "#E97304", "#C04E01", "#7D3200")),
+  CorpseBloom = list(c("#D8A574", "#E9C066", "#E4CF68", "#E79695", "#D46784", "#7C2A48")),
+  BoP = list(c("#5F6AB8", "#6E859A", "#FE924E", "#FFBC51", "#E3454C", "#EC7E94", "#1E552E", "#567C53")),
+  Stargazer = list(c("#C65327", "#921F3B", "#627B07", "#CA2863", "#FCC6FA")),
+  Plumeria = list(c("#89100B", "#D53C18", "#EC877B", "#ECBCB6", "#E4CBA8", "#E6D4C5")),
+  Sunflower = list(c("#301D11", "#6E5B4B", "#C69237", "#E69D1F", "#FBC95E", "#F9DF77")),
+  Dahlia = list(c("#57106C", "#B36DF3", "#D8AAF7", "#EFC9FF", "#C2C28C", "#6D9C5E", "#4D7F21", "#395721")),
+  Bluebell = list(c("#2B2496", "#595EC0", "#9B8DFE", "#BDBBFB", "#C4D2F1")),
+  Lily = list(c("#F4D06F", "#F2525C", "#C1CDE0", "#3B5B33", "#EDF38A", "#652A46", "#FA9000"))
+
+)
+
+#'Floral Palette Generator
+#' @param n Number of requested colors. If number exceeds availability in the given palette, colors are automatically interpolated. If n is not provided, the length of the palette is used.
+#' @param type Either "continuous" or "discrete". Use continuous if you wat to automatically interpolate between colors.
+#' @return A vector of colors
+#'
+#'
+floral.brewer <- function(name, n, type = c("discrete", "continuous")){
+  palette <- floral_palettes[[name]]
+  if (is.null(palette)|is.numeric(name)){
+    stop("Error! No palette under such name")
+  }
+  if (missing(n)) {
+    n <- length(palette())
+  }
+  }
+}
